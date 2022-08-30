f98936cbf5ebb4343c70602f64bc7e1c77dc6f60
GabriellRossi <gabriel.rossi159@gmail.com>
2022-08-30 00:54:21 -0300

    fix: Corrigir ainda a lib swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,30 @@ window.addEventListener('click', function() {
     header.classList.remove('scroll')
   }
 })
+
+
+
+/* TESTIMONIALS CAROUSEL SLIDER SWIPER */
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
