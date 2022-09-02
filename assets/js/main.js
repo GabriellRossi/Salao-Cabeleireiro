4c2099544a251978f9508ff9431b84c8bd33f291
GabriellRossi <gabriel.rossi159@gmail.com>
2022-09-02 10:05:52 -0300

    Criado a funçao para backToTop, subir para topo do site e, criado funções para deixar o código limpo

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,10 +21,10 @@ for (const link of links) {
 
 /* Mudar o header da página quando der scroll */
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('click', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -32,7 +32,9 @@ window.addEventListener('click', function () {
     // Menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
+
+
 
 /* TESTIMONIALS CAROUSEL SLIDER SWIPER */
 
@@ -61,5 +63,29 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`, 
-  { ìnterval: 100 });
+  #contact .text, #contact .links
+  #footer .brand, footer .social
+  `,
+  { ìnterval: 100 }
+)
+
+/* BOTÃO VOLTAR PARA O TOPO */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+
+
+/* WHEN SCROLL */
+
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
